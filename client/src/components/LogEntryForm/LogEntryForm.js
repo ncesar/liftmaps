1e1f5d7674305f030e6c065cd57230a62b42e8e1
Ednaldo Cesar Nascimento Cordeiro <cesar.nascimento@philips.com>
2020-04-03 14:23:36 -0300

    Small update

--- client/src/components/LogEntryForm/LogEntryForm.js
+++ client/src/components/LogEntryForm/LogEntryForm.js
@@ -10,7 +10,6 @@ import {
   FormControl,
   FormControlLabel,
   Checkbox,
-  Button,
 } from '@material-ui/core';
 import { Send } from '@styled-icons/boxicons-solid/Send';
 import { LogEntryFormWrapper, StyledButton, StyledFeedback } from './styled';
@@ -30,7 +29,7 @@ const LogEntryForm = ({ location, onClose }) => {
 
   const { register, handleSubmit } = useForm();
 
-  const [userAccepted, setUserAccepted] = useState(false);
+  const [userAccepted, setUserAccepted] = useState(undefined);
 
   const onSubmit = async data => {
     try {
@@ -53,11 +52,7 @@ const LogEntryForm = ({ location, onClose }) => {
     onClose();
   }
   return (
-    <LogEntryFormWrapper
-      onSubmit={handleSubmit(onSubmit)}
-      className="entry-form"
-    >
-      {error && <h3 className="error">{error}</h3>}
+    <React.Fragment>
       {loading && (
         <StyledFeedback>
           Obrigado pela sua contribuição, em até um dia útil a sua marcação será
@@ -65,143 +60,152 @@ const LogEntryForm = ({ location, onClose }) => {
           <span role="img" aria-label="Carinha feliz">
             😊
           </span>
-          <Button
+          <StyledButton
             variant="contained"
             color="primary"
+            margin="dense"
             onClick={() => setUserAccepted(true)}
           >
             Confirmar envio
-          </Button>
+          </StyledButton>
         </StyledFeedback>
       )}
-      <TextField
-        name="title"
-        required
-        inputRef={register}
-        variant="outlined"
-        label="Nome do local"
-        margin="dense"
-        inputProps={{ maxLength: 20 }}
-      />
-      <FormControl variant="outlined" margin="dense">
-        <InputLabel id="demo-simple-select-outlined-label">
-          A categoria desse local é..
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-outlined-label"
-          id="demo-simple-select-outlined"
-          value={category}
-          onChange={e => setCategory(e.target.value)}
-          label="Esse local.."
-        >
-          <MenuItem value="Mantimentos 🛒">
-            Mantimentos
-            <span role="img" aria-label="Mantimentos">
-              &nbsp;🛒
-            </span>
-          </MenuItem>
-          <MenuItem value="Refeições 🍽">
-            Refeições
-            <span role="img" aria-label="Refeições">
-              &nbsp;🍽
-            </span>
-          </MenuItem>
-          <MenuItem value="Serviços 💼">
-            Serviços
-            <span role="img" aria-label="Serviços">
-              &nbsp;💼
-            </span>
-          </MenuItem>
-          <MenuItem value="Pets 🐶">
-            Pets
-            <span role="img" aria-label="Pets">
-              &nbsp;🐶
-            </span>
-          </MenuItem>
-          <MenuItem value="Outros 🛠">
-            Outros
-            <span role="img" aria-label="Outros">
-              &nbsp;🛠
-            </span>
-          </MenuItem>
-        </Select>
-        <input
-          name="category"
-          value={category}
-          ref={register}
-          readOnly
-          style={{ display: 'none' }}
+      <LogEntryFormWrapper
+        onSubmit={handleSubmit(onSubmit)}
+        className="entry-form"
+        style={userAccepted === false ? { display: 'none' } : {}}
+      >
+        {error && <h3 className="error">{error}</h3>}
+        <TextField
+          name="title"
+          required
+          inputRef={register}
+          variant="outlined"
+          label="Nome do local"
+          margin="dense"
+          inputProps={{ maxLength: 20 }}
         />
-      </FormControl>
-      <PlaceOptions registerRef={register} />
-      <DaysOfWork registerRef={register} />
-      <TextField
-        name="description"
-        rows={2}
-        multiline
-        inputRef={register}
-        label="Sobre o local(tempo de entrega, etc)"
-        variant="outlined"
-        margin="dense"
-        inputProps={{ maxLength: 60 }}
-      />
-      <TextField
-        name="website"
-        inputRef={register}
-        label="Site/Link"
-        variant="outlined"
-        margin="dense"
-        inputProps={{ maxLength: 35 }}
-      />
-      <InputMask
-        value={tel}
-        name="phone"
-        mask="(99) 99999-9999"
-        alwaysShowMask
-        onChange={e => setTel(e.target.value)}
-      >
-        {inputProps => (
-          <TextField
-            {...inputProps}
-            inputRef={register}
-            name="phone"
-            label="Whats/Telefone(DDD)"
-            variant="outlined"
-            margin="dense"
-            placeholder="(99) 9.9999-9999"
+        <FormControl variant="outlined" margin="dense">
+          <InputLabel id="demo-simple-select-outlined-label">
+            A categoria desse local é..
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-outlined-label"
+            id="demo-simple-select-outlined"
+            value={category}
+            onChange={e => setCategory(e.target.value)}
+            label="Esse local.."
+          >
+            <MenuItem value="Mantimentos 🛒">
+              Mantimentos
+              <span role="img" aria-label="Mantimentos">
+                &nbsp;🛒
+              </span>
+            </MenuItem>
+            <MenuItem value="Refeições 🍽">
+              Refeições
+              <span role="img" aria-label="Refeições">
+                &nbsp;🍽
+              </span>
+            </MenuItem>
+            <MenuItem value="Serviços 💼">
+              Serviços
+              <span role="img" aria-label="Serviços">
+                &nbsp;💼
+              </span>
+            </MenuItem>
+            <MenuItem value="Pets 🐶">
+              Pets
+              <span role="img" aria-label="Pets">
+                &nbsp;🐶
+              </span>
+            </MenuItem>
+            <MenuItem value="Outros 🛠">
+              Outros
+              <span role="img" aria-label="Outros">
+                &nbsp;🛠
+              </span>
+            </MenuItem>
+          </Select>
+          <input
+            name="category"
+            value={category}
+            ref={register}
+            readOnly
+            style={{ display: 'none' }}
           />
-        )}
-      </InputMask>
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={checked}
-            name="isWhatsapp"
-            onChange={() => setChecked(!checked)}
-            color="primary"
-            inputRef={register}
-          />
-        }
-        label="É whatsapp?"
-        style={{ marginTop: '-10px' }}
-      />
-      <StyledButton
-        disabled={loading}
-        variant="contained"
-        color="primary"
-        margin="dense"
-        type="submit"
-      >
-        {loading ? (
-          'Carregando...'
-        ) : (
-          <span>
-            Adicionar local
-            <Send className="icon" style={{ marginLeft: 5 }} />
-          </span>
-        )}
-      </StyledButton>
-    </LogEntryFormWrapper>
+        </FormControl>
+        <PlaceOptions registerRef={register} />
+        <DaysOfWork registerRef={register} />
+        <TextField
+          name="description"
+          rows={2}
+          multiline
+          inputRef={register}
+          label="Sobre o local(tempo de entrega, etc)"
+          variant="outlined"
+          margin="dense"
+          inputProps={{ maxLength: 60 }}
+        />
+        <TextField
+          name="website"
+          inputRef={register}
+          label="Site/Link"
+          variant="outlined"
+          margin="dense"
+          inputProps={{ maxLength: 35 }}
+        />
+        <InputMask
+          value={tel}
+          name="phone"
+          mask="(99) 99999-9999"
+          alwaysShowMask
+          onChange={e => setTel(e.target.value)}
+        >
+          {inputProps => (
+            <TextField
+              {...inputProps}
+              inputRef={register}
+              name="phone"
+              label="Whats/Telefone(DDD)"
+              variant="outlined"
+              margin="dense"
+              placeholder="(99) 9.9999-9999"
+            />
+          )}
+        </InputMask>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={checked}
+              name="isWhatsapp"
+              onChange={() => setChecked(!checked)}
+              color="primary"
+              inputRef={register}
+            />
+          }
+          label="É whatsapp?"
+          style={{ marginTop: '-10px' }}
+        />
+        <StyledButton
+          disabled={loading}
+          variant="contained"
+          color="primary"
+          margin="dense"
+          type="submit"
+          onClick={() => setUserAccepted(false)}
+        >
+          {loading ? (
+            'Carregando...'
+          ) : (
+            <span>
+              Adicionar local
+              <Send className="icon" style={{ marginLeft: 5 }} />
+            </span>
+          )}
+        </StyledButton>
+      </LogEntryFormWrapper>
+    </React.Fragment>
   );
 };
 
